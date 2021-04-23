a480285332b9d3d8b91b1e8f9e53a2238c27eaf1
venkatesh <venkatesh9553361@gmail.com>
2021-04-23 09:13:55 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+mybutton = document.getElementById("totop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
